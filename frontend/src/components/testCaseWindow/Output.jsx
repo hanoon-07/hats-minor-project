58250e05cd8685256b58cfe0b87684a208988d9d
tsn67 <tomsebastian67@gmail.com>
2025-01-25 20:51:36 +0530

    stable deploy, updated testcase window

--- frontend/src/components/testCaseWindow/Output.jsx
+++ frontend/src/components/testCaseWindow/Output.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Editor from '@monaco-editor/react'
 import { useSelector } from 'react-redux'
 import { selectResult } from '../../redux/examSelector'
@@ -7,28 +7,42 @@ function Output() {
 
   const results = useSelector(selectResult);
 
-  function getItem(item, index) {
-    return <div key={item} className='bg-secondaryGray width-full m-[20px] rounded-[4px] px-2'>
-      <span className='text-textGray'>case {index}</span>
-      <p className='text-white'>{item}</p>
+  const errorOccured = useCallback(() => {
+    
+    for(let i = 0;i < results.stdErr.length;i++) {
+      if(results.stdErr[i] != null) return i;
+    }
+    return -1;
+
+  }, [results]);
+
+  function  getItem(item ,index) {
+    return <div className='flex flex-col gap-1'>
+      <p className='text-textGray'>case {index+1}</p>
+      <p className='whitespace-pre-wrap text-textGreen'>{item}</p>
     </div>
   }
 
-  if(!results) {
+
+  if(results == null) {
     return <div className=" text-center relative">
       <p className="text-gray-300">Run the Code to Obtain Results</p>
       <span className="w-[40%] h-[1px] left-[30%] bg-red-600 absolute "> </span>
     </div>
   }
 
-  return (
-    <div style={{ height: '100%', width: '100%'}} className='p-[15px]'>
-      {results.stdErr.length != 0 && <p className='overflow-y-scroll text-textRed px-[15px]'>{results.stdErr[0]}</p>}
-      {results.stdOut[0] != null && <div className='overflow-scroll overflow-y-scroll max-h-[200px]'>
-        {results.stdOut.map(getItem)}  
-      </div>}
+  if(errorOccured() != -1) {
+    return <div className='bg-darkGray w-full p-0 h-[100%]'>
+      <div className='h-full bg-black rounded-md'>
+        <p className='p-4  rounded-md text-red-500 whitespace-pre-wrap w-full'>{results.stdErr[errorOccured()]}</p>
+      </div>
     </div>
-  )
+  }
+
+  return <div className='bg-black rounded-md w-full p-3 max-h-[80%] min-h-[70%] overflow-y-scroll'>
+    {results.stdOut.map(getItem)}
+  </div>
+
 }
 
 export default Output
