8045bc91c0c271120905519f0f9ea0b8d308f6a6
tsn67 <tomsebastian67@gmail.com>
2025-01-27 19:32:16 +0530

    merge-main-no-conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './styles/index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import Textarea from './components/Textarea'
-
+import Exampage from './pages/Exampage'
 
 
 const App = () => {
