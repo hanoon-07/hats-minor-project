5a52de3b1d1276c07e117cdaeae94d8b15c34bf7
Amarnath <amarnathppp431@gmail.com>
2025-03-22 08:30:29 +0530

    monday-1

--- frontend/src/components/testCaseWindow/TestCase.jsx
+++ frontend/src/components/testCaseWindow/TestCase.jsx
@@ -1,9 +1,8 @@
-
 import {useState} from "react";
 import Button from "../Button";
-import { nanoid } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
-import { selectInputs, selectOutputs } from "../../redux/examSelector";
+import {nanoid} from "@reduxjs/toolkit";
+import {useSelector} from "react-redux";
+import {selectInputs, selectOutputs} from "../../redux/examSelector";
 
 /* const cases = [
   {enemyEngines: "1 2 3", currentEnergy: "4", output: "7"},
@@ -19,44 +18,51 @@ function TestCase() {
     setIndex(i);
   };
 
-  const selected = useSelector((state) => state['exam-data'].selected);
+  const selected = useSelector((state) => state["exam-data"].selected);
   const testCases = useSelector(selectInputs);
   const outputs = useSelector(selectOutputs);
 
   return (
-    <div className="px-1  pt-1 pb-1  overflow-y-scroll min-h-[40%] max-h-[90%]  scroller mb-4 h-full">
-      
+    <div className="px-1  pt-1 pb-1 scroller overflow-y-scroll min-h-[40%] max-h-[90%]  scroller mb-4 h-full">
       <section className="flex flex-wrap gap-4 items-center">
         {testCases.map((item, i) => (
-          <Button key={i} action={() => {handleButtonClick(i)}} label={`Case ${i+1}`} buttonClass={` text-textGray ${index == i?" border border-[#33E775]":""}`}/>
+          <Button
+            key={i}
+            action={() => {
+              handleButtonClick(i);
+            }}
+            label={`Case ${i + 1}`}
+            buttonClass={` text-textGray ${
+              index == i ? " border border-[#33E775]" : ""
+            }`}
+          />
         ))}
       </section>
 
       <section className="mt-5 flex flex-col  text-sm gap-4">
         {testCases.map((item, i) => {
           //console.log(selected);
-          if(i == selected) {
-            
-            return <div key={item}>
-              <div
-                  className=" flex flex-col p-4 gap-2 rounded-md bg-black"  
-              >
+          if (i == selected) {
+            return (
+              <div key={item}>
+                <div className=" flex flex-col p-4 gap-2 rounded-md bg-black">
                   <p className="text-textGray">{"input"}</p>
-                  <p className="py-1 rounded-[4px] text-[#33E775] font-normal whitespace-pre-line">{testCases[index]}</p>
-              </div>
+                  <p className="py-1 rounded-[4px] text-[#33E775] font-normal whitespace-pre-line">
+                    {testCases[index]}
+                  </p>
+                </div>
 
-              <div
-                  className=" flex mt-3 flex-col p-4 gap-2 rounded-md bg-black "
-                  >
+                <div className=" flex mt-3 flex-col p-4 gap-2 rounded-md bg-black ">
                   <p className="text-textGray">{"output"}</p>
-                  <p className="py-1 rounded-[4px] text-[#33E775] font-normal whitespace-pre-line">{outputs[index]}</p>
-              </div>        
-          </div>
+                  <p className="py-1 rounded-[4px] text-[#33E775] font-normal whitespace-pre-line">
+                    {outputs[index]}
+                  </p>
+                </div>
+              </div>
+            );
           }
-
         })}
       </section>
-
     </div>
   );
 }
