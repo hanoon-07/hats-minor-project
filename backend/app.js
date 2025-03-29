62daed1707c7eabd01308b82e5367033d0f609ea
tom-sebastian-67 <tomsebastian67@gmail.com>
2025-03-29 19:38:41 +0530

    stable v3

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5174",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
     "Content-Type",
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.port || 3000;
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5175", // Allow frontend
+        origin: "http://localhost:5174", // Allow frontend
         methods: ["GET", "POST"],
         credentials: true
     }
